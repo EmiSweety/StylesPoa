ed2a31393aaefa86253bc79dee8b6042801d18d9
hatako <grandrake10@gmail.com>
2021-02-22 16:39:42 -0500

    Tooltip a toolbar de vistas

--- js/view/view.base.js
+++ js/view/view.base.js
@@ -732,13 +732,13 @@ ui.directive('uiViewSwitcher', function(){
     template:
     '<div class="view-switcher pull-right hidden-phone">'+
         '<div class="btn-group">'+
-          '<button class="btn" x-view-type="grid"><i class="fa fa-list"></i></button>'+
+          '<button class="btn" x-view-type="grid"><i class="fa fa-list" title= "Vista Grilla" ></i></button>'+
         '<button class="btn" x-view-type="cards"><i class="fa fa-th-large"></i></button>'+
         '<button class="btn" x-view-type="kanban"><i class="fa fa-columns"></i></button>'+
           '<button class="btn" x-view-type="calendar"><i class="fa fa-calendar"></i></button>'+
           '<button class="btn" x-view-type="gantt"><i class="fa fa-calendar"></i></button>'+
           '<button class="btn" x-view-type="chart"><i class="fa fa-bar-chart-o"></i></button>'+
-          '<button class="btn" x-view-type="form"	><i class="fa fa-file-text-o"></i></button>'+
+          '<button class="btn" x-view-type="form"	><i class="fa fa-file-text-o"  title= "Vista Formulario" ></i></button>'+
         '</div>'+
     '</div>'
   };
